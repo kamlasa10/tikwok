9a927613caec43429fddb57c376172e3ff056461
Vlad <kamlasa@ukr.net>
2021-05-16 19:14:22 +0300

    finish

--- src/assets/scripts/gulp-modules/index.js
+++ src/assets/scripts/gulp-modules/index.js
@@ -1,6 +1,34 @@
 @@include('./libs.js');
 
 (function ($) {
+	if (screen.width > 700) {
+
+		$('.js-advice-open-slider').slick({
+			slidesToShow: 3,
+			slidesToScroll: 1,
+			dots: false,
+			arrows: false,
+			infinite: false,
+			responsive: [
+				{
+					breakpoint: 993,
+					settings: {
+						slidesToShow: 2,
+						slidesToScroll: 1,
+					}
+				},
+			]
+		});
+
+		$('.js-products-card-advice-sl-slider-next').click(function () {
+			$('.js-advice-open-slider').slick('slickPrev');
+		})
+
+		$('.js-products-card-advice-sl-slider-prev').click(function () {
+			$('.js-advice-open-slider').slick('slickNext');
+		});
+	}
+
 	if ($('.js-object-slider')){
 		$('.js-object-slider').on("init", function(event, slick){
 			$('.js-services-slider-counter').text(parseInt(slick.currentSlide + 1) + ' / ' + slick.slideCount);
